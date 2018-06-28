76972415c86480ca6b2053bf0c806094f08f9f8a
Walid <jebaliiwalid@gmail.com>
2018-06-28 10:36:27 +0100

    Arkanoid
    Checkpoint

---
+++ Assets/Scripts/restart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class restart : MonoBehaviour {
+
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter2D (Collision2D col ) {
+		Application.LoadLevel (Application.loadedLevel);
+
+	}
+
+
+}
